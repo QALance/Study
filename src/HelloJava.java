2ac7b8d7b5de57fb32bb193a07c0853ed971fc9e
QALance <chernikov.a@mail.ru>
2020-08-17 22:35:41 +0300

    test 3

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class HelloJava {
     public static void main(String[] args){
-        System.out.print("Hello, World");
-        System.out.print("!");
-        System.out.print("!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите катет A:");
+        int A = in.nextInt();
+        System.out.print("Введите катет B:");
+        int B = in.nextInt();
+        double z;
+        z = Math.sqrt(A*A+B*B);
+        System.out.println("Длина гипотенузы:" + z);
     }
 }
